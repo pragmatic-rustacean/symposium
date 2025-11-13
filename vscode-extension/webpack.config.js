573f0b225dd92ee44b741de664df0667a3593a46
Niko Matsakis <niko@alum.mit.edu>
2025-11-13 08:58:16 -0500

    Revert "outdated"
    This reverts commit c64cff89d6964f82670864c089b19057ddba4690.

--- vscode-extension/webpack.config.js
+++ vscode-extension/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
+const path = require('path');
 
 /** @type {import('webpack').Configuration} */
-const extensionConfig = {
-  target: "node",
-  mode: "none",
+const config = {
+  target: 'node',
+  mode: 'none',
 
-  entry: "./src/extension.ts",
+  entry: './src/extension.ts',
   output: {
-    path: path.resolve(__dirname, "out"),
-    filename: "extension.js",
-    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
   },
   externals: {
-    vscode: "commonjs vscode",
+    vscode: 'commonjs vscode'
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
@@ -24,59 +24,16 @@ const extensionConfig = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader",
-            options: {
-              configFile: "src/webview/tsconfig.json",
-            },
-          },
-        ],
-      },
-    ],
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
   },
-  devtool: "nosources-source-map",
+  devtool: 'nosources-source-map',
   infrastructureLogging: {
     level: "log",
   },
 };
 
-/** @type {import('webpack').Configuration} */
-const webviewConfig = {
-  target: "web",
-  mode: "none",
-
-  entry: "./src/webview/main.ts",
-  output: {
-    path: path.resolve(__dirname, "out"),
-    filename: "webview.js",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-    fallback: {
-      path: false,
-      fs: false,
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devtool: "nosources-source-map",
-  infrastructureLogging: {
-    level: "log",
-  },
-};
-
-module.exports = [extensionConfig, webviewConfig];
+module.exports = config;
